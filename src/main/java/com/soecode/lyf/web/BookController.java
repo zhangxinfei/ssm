f54ff51e00de6ca4cc06333c9a7c8951a8fefbb9
zhangxinfei <zhangxinfei@qq.com>
2018-08-24 09:05:27 +0800

    [M]测试

--- src/main/java/com/soecode/lyf/web/BookController.java
+++ src/main/java/com/soecode/lyf/web/BookController.java
@@ -39,6 +39,8 @@ public class BookController {
 		return "list";// WEB-INF/jsp/"list".jsp
 	}
 
+
+	
 	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
 	private String detail(@PathVariable("bookId") Long bookId, Model model) {
 		if (bookId == null) {
